d2015debb2b256a42172ca9d426a8b12686a105d
Omar Abdal-salam <sphinx@192.168.1.2>
2024-05-05 20:59:59 +0300

    deployment

--- dist/utilties.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getS3ObjectUrl = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const getS3ObjectUrl = (key) => `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${key}`;
-exports.getS3ObjectUrl = getS3ObjectUrl;
